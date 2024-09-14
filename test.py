eca38b95fb34326dbd11dd2a547bd97a431c4220
Nenad005 <markonenadovic2005@gmail.com>
2024-09-14 03:36:35 +0200

    websocket setup and flask finished

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from tinydb import TinyDB, Query
+from flask import Flask
+from flask_socketio import SocketIO, join_room, leave_room, send
+import random
+from string import ascii_uppercase
 
-db = TinyDB('db.json')
-lead = Query()
+app = Flask(__name__)
+socketio = SocketIO(app)
 
-res = db.search(lead.id == '!4m7!3m6!1s0x475a6511c6303e3f:0xd559772bf65dfc1!8m2!3d44.8374154!4d20.4027238!16s%2Fg%2F11px31xdn8!19sChIJPz4wxhFlWkcRwd9lv3KXVQ0')
-
-print(res)
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
