ca0cec5d9f115c7370083b3a58b5066d06ac4d9b
Nenad <nenadovicluka2001@gmail.com>
2024-09-16 15:04:26 +0200

    added header and footer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,9 @@
-import { ScrollArea } from "@radix-ui/react-scroll-area";
-import Image from "next/image";
 
 export default function Home() {
 	return (
 		<div className="h-screen">
 			<main className="shadow-[inset_0_-2px_0_rgba(20,20,20,1)] h-full">
-				test
+				tes
 			</main>
 		</div>
   	);
--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,18 +1,12 @@
 export default function Footer(){
     return <>
-        <header className="h-16 w-full flex justify-center">
-            <nav className="w-[1280px] h-full flex justify-start items-center">
-                <a className="flex">
-                    <h1>LOGO</h1>
-                    <h1>GM Leads</h1>
-                </a>
-                <h1>Leads</h1>
-                <h1>Statistics</h1>
-                <a className="flex ml-auto">
-                    <h1>Username</h1>
-                    <h1>PFP</h1>
-                </a>
-            </nav>
-        </header>
+        <footer className="w-full flex justify-center shadow-[inset_0_-2px_0_rgba(20,20,20,1)]">
+            <div className="w-[1280px] flex justify-start items-center">
+                <p className="py-8 text-muted-foreground">
+                    Built by <a href="https://github.com/Nenad005" target="_blank" className="underline-offset-4 underline cursor-pointer">Nenad005</a>
+                    . The source code is available on <a href="https://github.com/Nenad005/google-maps-scraper" target="_blank" className="underline-offset-4 underline cursor-pointer">GitHub</a>.
+                </p>
+            </div>
+        </footer>
     </>
 }
